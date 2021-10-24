771c53e992d73eceb8e8bfb84ce27b83c1f029c3
Preeetham Baliga <pbaliga@microsoft.com>
2021-10-24 09:57:11 +0530

    Updated test name

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -17,7 +17,7 @@ class HTTPClient {
 
 class RemoteFeedLoaderTests: XCTestCase {
 
-    func test_init() {
+    func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClient()
         _ = RemoteFeedLoader()
 
