d7c4b348dafac6a747917c7eabcddebffac3da72
Preeetham Baliga <pbaliga@microsoft.com>
2021-10-24 09:37:08 +0530

    Create the feed item model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Preetham Baliga on 24/10/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
