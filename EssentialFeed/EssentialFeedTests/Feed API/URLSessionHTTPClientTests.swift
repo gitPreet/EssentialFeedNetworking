026dc5dead251ddc07740522c931599c4701f544
Preeetham Baliga <pbaliga@microsoft.com>
2021-10-30 21:54:49 +0530

    Remove redundant test

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -18,32 +18,52 @@ class URLSessionHTTPClient {
     func get(from url: URL) {
         session.dataTask(with: url) { _, _, _ in
 
-        }
+        }.resume()
     }
 }
 
 class URLSessionHTTPClientTests: XCTestCase {
 
-    func test_getFromURLCreatesDataTaskWithURL() {
+    func test_getFromURL_resumeDataTaskWithURL() {
 
         let url = URL(string: "https://a-url.com")!
         let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
 
         let sut = URLSessionHTTPClient(session: session)
         sut.get(from: url)
 
-        XCTAssertEqual(session.receivedURLs, [url])
+        XCTAssertEqual(task.resumeCallCount, 1)
     }
 
     private class URLSessionSpy: URLSession {
-        var receivedURLs = [URL]()
+
+        var stubs = [URL: URLSessionDataTask]()
+
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
+        }
 
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
 
-    private class FakeURLSessionDataTask: URLSessionDataTask {}
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+
+        override func resume() {
+
+        }
+    }
+
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+
+        var resumeCallCount = 0
+
+        override func resume() {
+            resumeCallCount +=  1
+        }
+    }
 }
     
