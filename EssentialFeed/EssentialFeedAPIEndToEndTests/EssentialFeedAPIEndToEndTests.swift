1ed5cf4276b6c6cf74149e233b1f5af4f0df3f42
Preeetham Baliga <pbaliga@microsoft.com>
2021-11-02 20:25:48 +0530

    Added memory leak check

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -26,10 +26,13 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
         }
     }
 
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
+
+        trackForMemoryLeak(instance: client, file: file, line: line)
+        trackForMemoryLeak(instance: loader, file: file, line: line)
 
         var expectedResult: LoadFeedResult?
         let exp = expectation(description: "Wait for load completion")
