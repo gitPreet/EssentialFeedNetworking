8c2bb9e001e185c4aa12fe7ce71ec7e6d0ef55f1
Preeetham Baliga <pbaliga@microsoft.com>
2022-01-14 20:31:38 +0530

    extract harcoded storeURL from the codable feed store production type making it an explicit dependency passed in by the test so far

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -37,7 +37,11 @@ class CodableFeedStore {
         }
     }
 
-    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    private let storeURL: URL
+
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
 
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         guard let data = try? Data(contentsOf: storeURL) else {
@@ -132,7 +136,8 @@ class CodableFeedStoreTests: XCTestCase {
     //MARK: - Helpers
 
     private func makeSUT(line: UInt = #line, file: StaticString = #file) -> CodableFeedStore {
-        let sut = CodableFeedStore()
+        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+        let sut = CodableFeedStore(storeURL: storeURL)
         trackForMemoryLeak(instance: sut, file: file, line: line)
         return sut
     }
