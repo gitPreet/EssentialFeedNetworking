aa447b70b94ec00e759d9da5e71c50f88f9909dd
Preeetham Baliga <pbaliga@microsoft.com>
2022-01-21 13:31:58 +0530

    Added documentation for HTTPClient as well

--- EssentialFeed/EssentialFeed/FeedAPI/HTTPClient.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/HTTPClient.swift
@@ -13,5 +13,8 @@ public enum HTTPClientResult {
 }
 
 public protocol HTTPClient {
+
+    /// The completion handler can be invoked in any thread.
+    /// The clients are responsible to dispatch to appropriate threads, if needed.
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
