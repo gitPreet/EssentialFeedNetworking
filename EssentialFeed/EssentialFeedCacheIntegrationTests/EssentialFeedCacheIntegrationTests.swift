d662aaa30eb3dc14ec7b24f056b14981ff6848fc
Preeetham Baliga <pbaliga@microsoft.com>
2022-01-31 20:16:21 +0530

    Ad essential feed cache integration test target

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Preetham Baliga on 31/01/22.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
